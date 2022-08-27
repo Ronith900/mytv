c34e46174bed9a534374d0b938fdff21ff32593e
Ronith <44728044+Ronith900@users.noreply.github.com>
2022-08-27 11:40:24 +0800

    lifting the state up

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,20 +1,16 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  // State is a special prop in react, which basically is an object which contains data the component needs
-  state = {
-    value: this.props.value,
-    tags: ["tag1", "tag2", "tag3"],
-  };
+  // State is a special prop in react, which basically is an object which holds data the component needs
 
   //Event handling,  a function/object reference will be passed to the button which is calling
   // we can use arrow function instead of bind, in this case it will refere to the current object, which is Counter
   //After the setSate function is called, react in sometime will call the render method and update the count value.
   // When passing argument to a function we can use inline onClick= {() => this.handleIncrement(product)}
-  handleIncrement = () => {
-    const { value } = this.state;
-    this.setState({ value: value + 1 });
-  };
+  // handleIncrement = () => {
+  //   const { value } = this.state;
+  //   this.setState({ value: value + 1 });
+  // };
 
   render() {
     //JSX expression which eventually gets compiled to call React.createElement, thats why we need to import react class at the top.
@@ -24,21 +20,30 @@ class Counter extends Component {
     return (
       <div className="m-2">
         <span className={this.renderBadgeColor()}>{this.formatCount()}</span>
-        <button className="btn btn-primary" onClick={this.handleIncrement}>
+        <button
+          className="btn btn-secondary"
+          onClick={() => this.props.onIncrement(this.props.counter)}
+        >
           Increment
+        </button>
+        <button
+          className="btn btn-danger m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
         </button>
       </div>
     );
   }
 
   formatCount() {
-    const { value } = this.state;
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 
   renderBadgeColor() {
     let badge = "badge m-2 bg-";
-    badge += this.state.value === 0 ? "warning" : "primary";
+    badge += this.props.counter.value === 0 ? "warning" : "primary";
     return badge;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
+  // props vs state
+  //props - include data that we give to a component, is read only
+  // state - includes that is data that is local/private to the component
+  //Component which owns the data should be the one modifying it
+  //Controlled components will doesnt have its own state, and will only recive data from the parent and will raise events
   state = {
     counters: [
       { id: 1, value: 9 },
@@ -10,11 +15,42 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
+
+  handleDelete = (counterid) => {
+    const filteredCounter = this.state.counters.filter(
+      (c) => c.id !== counterid
+    );
+    this.setState({ counters: filteredCounter });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((counter) => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters });
+  };
   render() {
     return (
       <div>
+        <button className="btn btn-secondary m-2" onClick={this.handleReset}>
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
         ))}
       </div>
     );
